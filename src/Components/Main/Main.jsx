cd0966b0892b01e2cf1da2014fadfdb79e4f3569
anouar <anouar.ab95@gmail.com>
2024-06-26 02:25:51 +0100

    frist

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../Navbar/NavBar'
+import Slider from '../Slider/Slider'
+
+export default function Main() {
+  return (
+    <div>
+    <NavBar/>
+    <Slider/>
+    </div>
+  )
+}
